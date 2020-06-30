d607491f61bb3d022534f6aa495f9a0598c41f81
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-06-30 23:26:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ cursor = conn.cursor()
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status = discord.Status.dnd, activity = discord.Game('Я бот Lyrics, напиши "+help" и получи список моих команд'))
+    await bot.change_presence(status = discord.Status.idle, activity = discord.Game('Я бот Lyrics, напиши "+help" и получи список моих команд'))
     print(f'          [Lyrics]')
     print(f"[Lyrics] Bot successfully launched!;")
     print(f"[Lyrics] Name: [{bot.user}];")
